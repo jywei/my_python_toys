7a23d389179bcdaf567b760ebb64603027852735
Roy Wei <roywjy@gmail.com>
2016-10-08 15:51:46 +0800

    update crawler

--- crawler.py
+++ crawler.py
@@ -43,11 +43,28 @@ def get_all_links(page):
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
+    index = []
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
-            union(tocrawl, get_all_links(get_page(page)))
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(tocrawl, get_all_links(content))
             crawled.append(page)
     return crawled
+
+index = []
+
+def add_to_index(index, keyword, url):
+  for entry in index:
+    if entry[0] == keyword:
+      entry[1].append(url)
+      return
+  index.append([keyword,[url]])
+
+def add_page_to_index(index, url, content):
+  words = content.split()
+  for word in words:
+    add_to_index(index, word, url)
 
 
