4b25c498abf2ad22d6d0ee8de381ce720aee94ea
Roy Wei <roywjy@gmail.com>
2016-10-12 23:59:27 +0800

    update stop_watch

--- stop_watch.py
+++ stop_watch.py
@@ -10,3 +10,10 @@ def spin_loop(n):
   i = 0
   while i < n:
     i += 1
+
+print(time_execution('spin_loop(1000)')[1])
+print(time_execution('spin_loop(10000)')[1])
+print(time_execution('spin_loop(100000)')[1])
+print(time_execution('spin_loop(10 ** 5)')[1])
+print(time_execution('spin_loop(10 ** 6)')[1])
+print(time_execution('spin_loop(10 ** 9)')[1])
