e398ea429b2af2885e275d7ea955ab592600cc28
Roy Wei <roywjy@gmail.com>
2016-10-13 23:36:40 +0800

    add lookup function in add_index.py

--- add_index.py
+++ add_index.py
@@ -22,6 +22,12 @@ def add_to_index(index, keyword, url):
       return
   index.append([keyword, [url]])
 
+def lookup(index, keyword):
+  for entry in index:
+    if entry[0] == keyword:
+      return entry[1]
+  return None
+
 
 
 #add_to_index(index,'udacity','http://udacity.com')
