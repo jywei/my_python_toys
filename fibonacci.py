f89a40afc283ca706c7d2c0cfc01ad15fcc07849
Roy Wei <roywjy@gmail.com>
2016-10-20 23:54:56 +0800

    improve fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -20,3 +20,17 @@ print(fibonacci(1))
 #>>> 1
 print(fibonacci(15))
 #>>> 610
+#
+
+#Define a faster fibonacci procedure that will enable us to computer
+#fibonacci(36).
+
+def fibonacci(n):
+    current = 0
+    after = 1
+    for i in range(0,n):
+        current, after = after, current + after
+    return current
+
+print(fibonacci(36))
+#>>> 14930352
