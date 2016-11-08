6e7084fec66421d8ef8d896637f99aab0069b058
Roy Wei <roywjy@gmail.com>
2016-11-08 23:59:58 +0800

    play with stings

--- string_formating.py
+++ string_formating.py
@@ -5,3 +5,18 @@ March       -{2}
 April       -{1}
 May         -{2}
 June        -{1}""".format(28, 30, 31))
+
+splitString = "This \nis Sparta!"
+print(splitString)
+
+tabbedString = "a\t b\t c\t d\t"
+print(tabbedString)
+
+print("A 'quote' inside another 'quote'")
+
+print("""
+Yo
+Check
+It
+out!
+""")
