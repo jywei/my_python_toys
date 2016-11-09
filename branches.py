220094cb157f28cef635f835a18749c9f1719d8b
Roy Wei <roywjy@gmail.com>
2016-11-09 23:55:25 +0800

    trying different printing functions

---
+++ branches.py
@@ -0,0 +1,17 @@
+humans = 20
+dinosours = 30
+fish = 35
+
+if humans < dinosours:
+    print("There are more dinosours than humans")
+
+if dinosours > fish:
+    print("Fish!")
+print("Dinosours rock!")
+
+if humans < dinosours:
+    print("Dinosours!!")
+elif humans > fish:
+    print("Humans!!!!!")
+else:
+    print("Nothing!")
